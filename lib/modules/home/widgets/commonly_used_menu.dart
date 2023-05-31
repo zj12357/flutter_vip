ef85467db43d192301e9be0b24c45d4fb48cb062
norman <zj15355208214@gmail.com>
2023-06-01 01:17:10 +0800

    头部样式

--- lib/modules/home/widgets/commonly_used_menu.dart
+++ lib/modules/home/widgets/commonly_used_menu.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/cupertino.dart';
+
+import '/shared/styles/background_style.dart';
 import '/shared/styles/text_style.dart';
+
+class NavItem {
+  final String icon;
+  final String label;
+  NavItem({required this.icon, required this.label});
+}
 
 class CommonlyUsedMenu extends StatefulWidget {
   const CommonlyUsedMenu({Key? key}) : super(key: key);
@@ -9,41 +17,66 @@ class CommonlyUsedMenu extends StatefulWidget {
 }
 
 class _CommonlyUsedMenuState extends State<CommonlyUsedMenu> {
+  final List<NavItem> menuList = [
+    NavItem(icon: 'assets/images/icon/transfer_icon.png', label: 'Home'),
+    NavItem(icon: 'assets/images/icon/transfer_icon.png', label: 'Search'),
+    NavItem(icon: 'assets/images/icon/transfer_icon.png', label: 'Favorites'),
+    NavItem(icon: 'assets/images/icon/transfer_icon.png', label: 'Settings'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Image(
-          image: AssetImage('assets/images/icon/transfer_icon.png'),
-          width: 33,
-          height: 32,
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Container(
+        color: BackgroundStyles.secondaryBackground.withOpacity(0.5),
+        child: SizedBox(
+          height: 75,
+          width: double.infinity,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: menuList.length,
+            itemBuilder: (context, index) {
+              final item = menuList[index];
+              return GestureDetector(
+                onTap: () {
+                  // 处理导航菜单项的点击事件
+                  // 根据需要执行相应的导航操作
+                  print('Tapped ${item.label}');
+                },
+                child: Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    child: CommonlyUsedMenuItem(menuItemProps: item),
+                  ),
+                ),
+              );
+            },
+          ),
         ),
-        SizedBox(
-          height: 10,
-        ),
-        Text(
-          'OK111',
-          style: TextStyle(fontSize: 12, color: TextStyles.whiteColor),
-        ),
-      ],
+      ),
     );
   }
 }
 
 class CommonlyUsedMenuItem extends StatelessWidget {
-  const CommonlyUsedMenuItem({Key? key}) : super(key: key);
+  final NavItem menuItemProps;
+  const CommonlyUsedMenuItem({Key? key, required this.menuItemProps})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Image(image: AssetImage('/assets/images/icon/transfer_icon.png')),
-        SizedBox(
-          height: 10,
+        Image.asset(
+          menuItemProps.icon,
+          width: 32,
         ),
+        const SizedBox(height: 4),
         Text(
-          'OK111',
-          style: TextStyle(fontSize: 16, color: TextStyles.whiteColor),
+          menuItemProps.label,
+          style: const TextStyle(fontSize: 16, color: TextStyles.whiteColor),
         ),
       ],
     );
--- lib/shared/styles/background_style.dart
+++ lib/shared/styles/background_style.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class BackgroundStyles {
   static const Color primaryBackground = Color(0xFF131415);
+  static const Color secondaryBackground = Color(0xFF242322);
 }
